1abede9a4aefc842ecad2fb153b539bd080e9766
Martinski95 <marti_kirilov@abv.bg>
2014-03-12 15:38:00 +0200

    Successfully sending the data(without image) from the web form to the java rest server.

--- InternetTechnologiesProject/WebContent/js/index.js
+++ InternetTechnologiesProject/WebContent/js/index.js
@@ -1,44 +1,16 @@
-$( document ).ready(function() {
-	$.Mustache.options.domTemplateType="text/x-mustache-template";
-	$.Mustache.addFromDom();
-	i18n.init({ lng: "bg", fallbackLng: "bg"}, function() {
-		$(document).trigger("onAjaxUpdateRequired");
-	});
-	
-	
-	
-	$(document).on("click", "#button_showRemoveMessage", function() {
-		var id = $(this).data('id');
-		
-		bootbox.confirm("Are you sure", function(result) {
-			if (result) {
-				executeRequest("api/hello/"+id, "DELETE", null, function(data) {
-					$(document).trigger("onAjaxUpdateRequired");
-				});
-			}
-		});
-	});
-	
-	$(document).on('click', '#dialog-message-create' ,function() {
-			var message = {
-				text: $("#dialog_message").find("#text").val()
-			};
-			executeRequest("api/hello/add", "POST", message, function(data) {
-				$(document).trigger("onAjaxUpdateRequired");
-				$('#dialog_message').modal('hide');
-			});
-
-	});
-	$(document).on('click', '#dialog-message-update' ,function() {
-		var message = {
-				id: $(this).data("id"),
-				text: $("#dialog_message").find("#text").val()
-			};
-
-			executeRequest("api/hello/update", "PUT", message, function(data) {
-				$(document).trigger("onAjaxUpdateRequired");
-				$('#dialog_message').modal('hide');
-			});
-	});
-	
+$("#button_send").click(function() {
+    var toSend = {
+    	latitude : $("#latitude").val(),
+    	longitude : $("#longitude").val(),
+    	address : $("#address").val(),
+    	message : $("#message").val()
+    };
+    
+    return $.ajax({
+    	url: "api/Rest/add",
+    	type: "POST",
+    	data: JSON.stringify(toSend),
+    	contentType: 'application/json; charset=utf-8',
+    	dataType: 'json'
+    });
 });
