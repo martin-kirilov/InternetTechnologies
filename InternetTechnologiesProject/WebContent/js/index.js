5f51eeefabd00badd9450fc66115ff9b709c36b4
Martinski95 <marti_kirilov@abv.bg>
2014-03-17 23:06:14 +0200

    Updated index.js to get the id from the new uploaded Complaint.

--- InternetTechnologiesProject/WebContent/js/index.js
+++ InternetTechnologiesProject/WebContent/js/index.js
@@ -1,3 +1,5 @@
+var id;
+
 $("#button_send").click(function() {
     var toSend = {
     	latitude : $("#latitude").val(),
@@ -6,14 +8,18 @@ $("#button_send").click(function() {
     	message : $("#message").val()
     };
     
-    return $.ajax({
+    var jqXHR =  $.ajax({
     	url: "api/Rest/add",
     	type: "POST",
     	data: JSON.stringify(toSend),
     	contentType: 'application/json; charset=utf-8',
-    	dataType: 'json'
+    	dataType: 'json',
+    	async: false
     });
+    alert(jQuery.parseJSON(jqXHR.responseText).id);
+    return jqXHR;
 });
+
 
 $("#button_getAll").click(function() {
 	return $.ajax({
@@ -23,3 +29,62 @@ $("#button_getAll").click(function() {
 		dataType: 'json'
 	});
 });
+
+
+
+(function($){
+	$(document).ready(function(){
+		$('#dragzone').bind('dragover',function(event){
+			$(event.target).css('background-color','red');
+			event.stopPropagation();
+			event.preventDefault();  
+		});
+		$('#dragzone').bind('drop',function(event){
+			$(event.target).css('background-color','white');
+			$.FileUpload($(event.target).attr('path'),event.originalEvent.dataTransfer.files);
+			event.stopPropagation();
+			event.preventDefault();
+		});
+	});
+})(jQuery);
+
+$.FileUpload = function(path,files) {
+
+	var file = files.item(0);
+	var xhr = new XMLHttpRequest();
+	
+	fileUpload = xhr.upload,
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState == 4) {
+			response = $.parseJson(xhr.responseText);
+			alert(response.status);
+		}
+	};
+		
+	/*
+	fileUpload.onprogress = function(e){
+		if (e.lengthComputable) {
+			var _progress = Math.round((e.loaded * 100) / e.total);
+			if (_progress != 100){
+					$('#upload_progress_'+0).text(_progress + '%');
+			}
+		}
+	};
+	*/
+	alert("Posting to server...");
+
+	var sessionid = $('html').attr('id');
+	alert (sessionid);
+
+	
+	xhr.open("POST", '/Rest/Complaints', true);
+	xhr.setRequestHeader("Cache-Control", "no-cache");
+	xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+	xhr.setRequestHeader("X-File-Name", encodeURIComponent(file.fileName));
+	xhr.setRequestHeader("X-File-Size", file.fileSize);
+	xhr.setRequestHeader("Content-Type", "application/octet-stream");
+	xhr.send(file);
+	
+	alert("After post to server...");
+
+};
